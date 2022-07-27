717a9626dcef367008544ac1127187b3849fe387
SlimTim10 <slimtim10@gmail.com>
2022-07-27 10:51:27 -0400

    Set up testing

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,6 @@
 // http://www.ai-junkie.com/ga/intro/gat3.html
 
-const eqArrays = (xs, ys) => (xs.length === ys.length) && xs.every((_, i) => {
-  if (Array.isArray(xs[i]) && Array.isArray(ys[i])) {
-    return eqArrays(xs[i], ys[i]);
-  } else {
-    return ys[i] === xs[i];
-  }
-});
+const R = require('ramda')
 
 const geneMap = {
   '0': [0,0,0,0],
@@ -25,12 +19,42 @@ const geneMap = {
   '/': [1,1,0,1]
 }
 
+// TYPES
+// Chromosome :: [Bit]
+// Gene :: (Bit, Bit, Bit, Bit)
+
 // Char -> Gene
 const charToGene = x => geneMap[x]
 
 // Gene -> Char
 const geneToChar = x => (
   Object.entries(geneMap)
-    .find(([k, v]) => eqArrays(x, v))[0]
+    .find(([k, v]) => R.equals(x, v))[0]
 )
 
+// Chromosome -> [Gene]
+const genes = x => R.splitEvery(4, x)
+
+// Chromosome -> Integer
+const decode = x => {
+  const gs = genes(x)
+}
+
+// (Chromosome, Integer) -> Maybe Float
+const fitness = (chrom, target) => {
+  const x = decode(chrom)
+  if (target - x === 0) return {nothing: true}
+  return {
+    nothing: false,
+    val: 1 / (target - x)
+  }
+}
+
+module.exports = {
+  geneMap,
+  charToGene,
+  geneToChar,
+  genes,
+  decode,
+  fitness
+}
